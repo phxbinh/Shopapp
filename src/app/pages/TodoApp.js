6f069ea9be651a9e1d02e82138201eb7a87e2c91
phxbinh <phxbinh@gmail.com>
2026-01-27 14:51:21 +0700

    Update TodoApp.js

--- src/app/pages/TodoApp.js
+++ src/app/pages/TodoApp.js
@@ -62,7 +62,9 @@ return h("div", { className: "todo-app" },
 
 // src/app/pages/TodoApp.js
 export function TodoApp({ data }) {
-  const [todos, setTodos] = useState(data?.todos || []);
+  const data = props.data || {};
+  const [todos] = useState(data.todos || []);
+  //const [todos, setTodos] = useState(data?.todos || []);
   const [input, setInput] = useState("");
 
   async function add() {
