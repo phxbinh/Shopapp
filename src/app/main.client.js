dfb52ff206516f6239cf915148b8dbd9ced615a0
phxbinh <phxbinh@gmail.com>
2026-01-27 14:41:08 +0700

    Update main.client.js

--- src/app/main.client.js
+++ src/app/main.client.js
@@ -13,5 +13,32 @@ import { TodoApp } from "./pages/TodoApp.js";
 
 const { Router } = window.App;
 
-Router.addRoute("/", TodoApp);
+//Router.addRoute("/", TodoApp);
+
+Router.addRoute({
+  path: "/",
+  component: TodoApp,
+/*
+  loader: async () => {
+    const todos = await fetchTodos();
+    return { todos };
+  }
+*/
+loader: async () => {
+  if (window.__CACHE__?.todos) {
+    return window.__CACHE__.todos;
+  }
+
+  const todos = await fetchTodos();
+  window.__CACHE__ = { todos };
+  return { todos };
+}
+
+});
+
+
+
+
+
+
 Router.init(document.getElementById("app"), { hash: false });
