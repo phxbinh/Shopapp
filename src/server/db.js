6b9473d425dca92a7ff6066315602ea768f99754
phxbinh <phxbinh@gmail.com>
2026-01-27 16:34:15 +0700

    Update db.js

--- src/server/db.js
+++ src/server/db.js
@@ -1,25 +1,5 @@
-/*
+
 // src/server/db.js
-import { sql } from "@neondatabase/serverless";
-
-export async function getTodos() {
-  const { rows } = await sql`
-    SELECT id, text
-    FROM todos
-    ORDER BY id DESC
-  `;
-  return rows;
-}
-
-export async function addTodo(text) {
-  await sql`INSERT INTO todos (text) VALUES (${text})`;
-}
-
-export async function deleteTodo(id) {
-  await sql`DELETE FROM todos WHERE id = ${id}`;
-}
-*/
-
 import { neon } from "@neondatabase/serverless";
 
 const sql = neon(process.env.DATABASE_URL);
