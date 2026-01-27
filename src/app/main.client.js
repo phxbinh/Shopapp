f975afde224cb8d0a58c6e29c1cc5fafabb6e5eb
phxbinh <phxbinh@gmail.com>
2026-01-27 12:40:52 +0700

    Update main.client.js

--- src/app/main.client.js
+++ src/app/main.client.js
@@ -4,6 +4,14 @@ import '../framework/hooks.js';
 import '../framework/router.js';
 //import '../framework/init_API.js';
 
+/*
 import { TodoApp } from './pages/TodoApp.js';
-
 window.App.VDOM.render(TodoApp, document.getElementById('app'));
+*/
+
+import { TodoApp } from "./pages/TodoApp.js";
+
+const { Router } = window.App;
+
+Router.addRoute("/", TodoApp);
+Router.init(document.getElementById("app"), { hash: false });
