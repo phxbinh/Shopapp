ed0df53a80b698e6964f54c5d04aec34fc4af20a
phxbinh <phxbinh@gmail.com>
2026-01-28 13:17:30 +0700

    Update api.js

--- src/shared/api.js
+++ src/shared/api.js
@@ -1,5 +1,6 @@
 // src/shared/api.js
 export async function fetchTodos() {
+  alert("Fetch data to DB")
   const res = await fetch("/api/todos");
   if (!res.ok) throw new Error("Fetch todos failed");
   return res.json();
